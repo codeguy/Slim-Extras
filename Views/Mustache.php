4a70fc1c6063ffdb5232f6bcdf06eb4ff926109d
hanseartic <schuette.paul@gmail.com>
2012-10-05 13:19:38 +0200

    Make use of Mustache-Autoloader and take the change from 'Mustache' to 'Mustache_Engine' into account.

--- Views/Mustache.php
+++ Views/Mustache.php
@@ -58,10 +58,10 @@ class Mustache extends \Slim\View
      */
     public function render($template)
     {
-        require_once self::$mustacheDirectory . '/Mustache.php';
-        $m = new \Mustache();
+        require_once self::$mustacheDirectory . '/Autoloader.php';
+        \Mustache_Autoloader::register();
+        $m = new \Mustache_Engine();
         $contents = file_get_contents($this->getTemplatesDirectory() . '/' . ltrim($template, '/'));
-
         return $m->render($contents, $this->data);
     }
 }
