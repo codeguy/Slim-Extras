dfc10a5576e830a9a8177115396e14076e322883
hanseartic <schuette.pual@gmail.com>
2012-10-05 13:38:04 +0200

    Make the autoloader only work the mustache-directory.

--- Views/Mustache.php
+++ Views/Mustache.php
@@ -59,7 +59,7 @@ class Mustache extends \Slim\View
     public function render($template)
     {
         require_once self::$mustacheDirectory . '/Autoloader.php';
-        \Mustache_Autoloader::register();
+        \Mustache_Autoloader::register(dirname(self::$mustacheDirectory));
         $m = new \Mustache_Engine();
         $contents = file_get_contents($this->getTemplatesDirectory() . '/' . ltrim($template, '/'));
         return $m->render($contents, $this->data);
